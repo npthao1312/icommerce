080d4c8075fa471da549e7c782d14ec04186fe6d
npthao1312 <npthao1312@gmail.com>
2021-11-09 15:48:46 +0700

    Add Delete one & all products

--- src/main/java/com/ecommerce/controllers/ProductController.java
+++ src/main/java/com/ecommerce/controllers/ProductController.java
@@ -2,6 +2,7 @@ package com.ecommerce.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,9 +20,11 @@ import com.ecommerce.repository.ProductRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.constraints.NotNull;
 
@@ -54,8 +57,10 @@ public class ProductController {
 		}
 	}
 
-	@Operation(summary = "Create a product", tags = { "Product" })
+	@Operation(summary = "Create a product", security = { @SecurityRequirement(name = "bearer-key") }, tags = {
+			"Product" })
 	@ApiResponses(value = @ApiResponse(description = "successful operation"))
+	@PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
 	@PostMapping(value = { "" })
 	public ResponseEntity<Product> createProduct(@RequestBody Product product) {
 		try {
@@ -64,6 +69,45 @@ public class ProductController {
 			return new ResponseEntity<>(_product, HttpStatus.CREATED);
 		} catch (Exception e) {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Product> getProductById(@PathVariable("id") long id) {
+		Optional<Product> productData = productRepository.findById(id);
+
+		if (productData.isPresent()) {
+			return new ResponseEntity<>(productData.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@Operation(summary = "Delete a product", security = { @SecurityRequirement(name = "bearer-key") }, tags = {
+			"Product" })
+	@ApiResponses(value = @ApiResponse(description = "successful operation"))
+	@PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
+	@DeleteMapping("/{id}")
+	public ResponseEntity<HttpStatus> deleteProduct(@PathVariable("id") long id) {
+		try {
+			productRepository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@Operation(summary = "Delete all product", security = { @SecurityRequirement(name = "bearer-key") }, tags = {
+			"Product" })
+	@ApiResponses(value = @ApiResponse(description = "successful operation"))
+	@PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
+	@DeleteMapping("")
+	public ResponseEntity<HttpStatus> deleteAllProducts() {
+		try {
+			productRepository.deleteAll();
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 }
